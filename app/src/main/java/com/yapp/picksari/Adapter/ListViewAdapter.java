75d597957d6ef7d82ecf424d3a4e21e084238bc1
YERIM94 <shareetc_@naver.com>
2018-05-26 18:09:31 +0900

    no message

--- app/src/main/java/com/yapp/picksari/Adapter/ListViewAdapter.java
+++ app/src/main/java/com/yapp/picksari/Adapter/ListViewAdapter.java
@@ -33,16 +33,16 @@ import static com.yapp.picksari.Adapter.MyCursorAdapter.musicdb;
  * Created by myeong on 2018. 4. 2..
  */
 
-public class ListViewAdapter extends ArrayAdapter<musicItem>{
+public class ListViewAdapter extends ArrayAdapter<musicItem> {
 
     public static List<musicItem> items;
     static ImageButton btnPick;
     int realPosition = 0;
-     static SQLiteDatabase musicdb;
-     static DBhelper musichelper;
+    static SQLiteDatabase musicdb;
+    static DBhelper musichelper;
 
     public ListViewAdapter(Context context, int textViewResourceId) {
-        super(context,textViewResourceId);
+        super(context, textViewResourceId);
     }
 
     public ListViewAdapter(Context context, int resource, List<musicItem> items) {
@@ -57,89 +57,104 @@ public class ListViewAdapter extends ArrayAdapter<musicItem>{
 
         View view = convertView;
 
-        final musicItem music = items.get(position);
-        if (view == null) {
-            LayoutInflater vi;
-            vi = LayoutInflater.from(getContext());
-            view = vi.inflate(R.layout.activity_listview, null);
-        }
-
-        realPosition = position;
-
-        Button genre = (Button) view.findViewById(R.id.tvGenre);
-        Button octave = (Button) view.findViewById(R.id.tvOctave);
-        TextView title = (TextView) view.findViewById(R.id.tvTitle);
-        TextView singer = (TextView) view.findViewById(R.id.tvSinger);
-        genre.setText(music.mGenre);
-        octave.setText(music.mOctave);
-        title.setText(music.mName);
-        singer.setText(music.mSinger);
-
-        final SQLiteDatabase musicdb;
-        final DBhelper musichelper;
-        musichelper = MyPickFragment.myHelper;
-        musicdb = musichelper.getReadableDatabase();
-
-        btnPick = (ImageButton)view.findViewById(R.id.btnPick);
-
-
-        // 시작 시 노란 하트로 보이게 하려고 디비에서 불러옴
-        Cursor cursor = musicdb.rawQuery("SELECT mName, mSinger FROM " + musichelper.TABLE_NAME + ";", null);
-
-        while (cursor.moveToNext()) {
-            if (cursor.getString(0).equals(music.mName) && cursor.getString(1).equals(music.mSinger)) {
-                btnPick.setImageResource(R.drawable.ic_heart_symbol_yellow);
-                music.mPick = 1;
+        try {
+            final musicItem music = items.get(position);
+            if (view == null) {
+                LayoutInflater vi;
+                vi = LayoutInflater.from(getContext());
+                view = vi.inflate(R.layout.activity_listview, null);
             }
-        }
 
-        // 하트 누르자마자 리스트뷰 바로 갱신하기 위해서
-        if (music.mPick == 1) {
-            btnPick.setImageResource(R.drawable.ic_heart_symbol_yellow);
-        }
-        if (music.mPick == 0) {
-            btnPick.setImageResource(R.drawable.ic_search_heart_lightgray);
-        }
+            realPosition = position;
+
+            Button genre = (Button) view.findViewById(R.id.tvGenre);
+            Button octave = (Button) view.findViewById(R.id.tvOctave);
+            TextView title = (TextView) view.findViewById(R.id.tvTitle);
+            TextView singer = (TextView) view.findViewById(R.id.tvSinger);
+            genre.setText(music.mGenre);
+            octave.setText(music.mOctave);
+            title.setText(music.mName);
+            singer.setText(music.mSinger);
+
+            final SQLiteDatabase musicdb;
+            final DBhelper musichelper;
+            musichelper = MyPickFragment.myHelper;
+            musicdb = musichelper.getReadableDatabase();
+
+            btnPick = (ImageButton) view.findViewById(R.id.btnPick);
 
 
-        btnPick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 하트 클릭시 db에 추가
-                if (music.mPick == 0) {
-                    btnPick.setImageAlpha(R.drawable.ic_heart_symbol_yellow);
+            // 시작 시 노란 하트로 보이게 하려고 디비에서 불러옴
+            Cursor cursor = musicdb.rawQuery("SELECT mName, mSinger FROM " + musichelper.TABLE_NAME + ";", null);
 
-                    ContentValues row = new ContentValues();
-                    row.put("mName", music.mName);
-                    row.put("mSinger", music.mSinger);
-                    row.put("mGenre", music.mGenre);
-                    row.put("mOctave", music.mOctave);
-                    row.put("mPick", getPosition(music)); // 홈 리스트의 실제 위치 정보 저장
-
-                    musicdb.insert(musichelper.TABLE_NAME, null, row);
-                    MyPickFragment.myAdapter.notifyDataSetChanged();
-
-                    HomeFragment.get_reset();
-                    MyPickFragment.get_reset();
-
+            while (cursor.moveToNext()) {
+                if (cursor.getString(0).equals(music.mName) && cursor.getString(1).equals(music.mSinger)) {
+                    btnPick.setImageResource(R.drawable.ic_heart_symbol_yellow);
                     music.mPick = 1;
                 }
-                else if (music.mPick == 1){
-                    // 하트 클릭시 db에서 삭제
-                    btnPick.setImageAlpha(R.drawable.ic_search_heart_lightgray);
-
-                    musicdb.execSQL("DELETE FROM " + musichelper.TABLE_NAME + " WHERE mName = '" + music.mName + "' AND mSinger = '" + music.mSinger + "';");
-                    MyPickFragment.myAdapter.notifyDataSetChanged();
-
-                    HomeFragment.get_reset();
-                    MyPickFragment.get_reset();
-
-                    music.mPick = 0;
-                }
-                notifyDataSetChanged();
             }
-        });
 
-        return view;
+            // 하트 누르자마자 리스트뷰 바로 갱신하기 위해서
+            if (music.mPick == 1) {
+                btnPick.setImageResource(R.drawable.ic_heart_symbol_yellow);
+            }
+            if (music.mPick == 0) {
+                btnPick.setImageResource(R.drawable.ic_search_heart_lightgray);
+            }
+
+
+            btnPick.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 하트 클릭시 db에 추가
+                    if (music.mPick == 0) {
+                        btnPick.setImageAlpha(R.drawable.ic_heart_symbol_yellow);
+
+                        ContentValues row = new ContentValues();
+                        row.put("mName", music.mName);
+                        row.put("mSinger", music.mSinger);
+                        row.put("mGenre", music.mGenre);
+                        row.put("mOctave", music.mOctave);
+                        row.put("mPick", getPosition(music)); // 홈 리스트의 실제 위치 정보 저장
+
+                        musicdb.insert(musichelper.TABLE_NAME, null, row);
+                        MyPickFragment.myAdapter.notifyDataSetChanged();
+                        try {
+                            HomeFragment.get_reset();
+                        } catch (Exception e) {
+                        }
+                        try {
+                            MyPickFragment.get_reset();
+                        } catch (Exception e) {
+                        }
+
+                        music.mPick = 1;
+                    } else if (music.mPick == 1) {
+                        // 하트 클릭시 db에서 삭제
+                        btnPick.setImageAlpha(R.drawable.ic_search_heart_lightgray);
+
+                        musicdb.execSQL("DELETE FROM " + musichelper.TABLE_NAME + " WHERE mName = '" + music.mName + "' AND mSinger = '" + music.mSinger + "';");
+                        MyPickFragment.myAdapter.notifyDataSetChanged();
+
+                        try {
+                            HomeFragment.get_reset();
+                        } catch (Exception e) {
+                        }
+                        try {
+                            MyPickFragment.get_reset();
+                        } catch (Exception e) {
+                        }
+
+                        music.mPick = 0;
+                    }
+                    notifyDataSetChanged();
+                }
+            });
+        }
+        catch (Exception e) {
+        }
+            return view;
+
+
     }
 }
